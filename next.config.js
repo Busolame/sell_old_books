203c8689d945a4d4c04864853afc629ffd2f44f5
busolami <busolaazeez@gmail.com>
2025-01-22 23:45:10 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
       },
+      env: {
+        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      },
+    
 }
 
 module.exports = nextConfig
